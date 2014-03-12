d02fea55d7fcd80f6504a75beef416bd5856259e
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-03-12 13:16:21 +0900

    initial commit n bundleinstall n setting sass

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onken::Application.config.secret_token = '67b6601f71cec8bc04f432396339e430ca2ad725acff01f2a817e7449c8c954f51a22ab5f9671a590d634e45ff83076dcdc952016cd9980f79201e5dfb94d6c9'
